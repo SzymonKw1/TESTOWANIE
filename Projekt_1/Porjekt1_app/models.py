eacf3029726e719425e564463755a2acce6f4cba
SzymonKw1 <skwiatkowski3@st.swps.edu.pl>
2024-10-23 12:03:16 +0200

    Klasa Zawodnik

--- Projekt_1/Porjekt1_app/models.py
+++ Projekt_1/Porjekt1_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Zawodnik(models.Model):
+    imie_i_nazwisko = models.CharField(max_length=100)
+    pozycja = models.CharField(max_length=50)
+    numer = models.IntegerField()
+    wiek = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.imie_i_nazwisko} ({self.numer}) - {self.pozycja}'
+
 # Create your models here.
