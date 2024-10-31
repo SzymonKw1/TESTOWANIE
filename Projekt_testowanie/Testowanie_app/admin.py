22fcf4c07a72a03c4a78232c7ed21d3eb1149b80
SzymonKw1 <skwiatkowski3@st.swps.edu.pl>
2024-10-31 11:37:10 +0100

    Wykonanie wszystkich pozostałych zadań z Lab_04

--- Projekt_testowanie/Testowanie_app/admin.py
+++ Projekt_testowanie/Testowanie_app/admin.py
@@ -4,3 +4,6 @@ from django.contrib import admin
 from .models import Osoba, Stanowisko
 admin.site.register(Osoba)
 admin.site.register(Stanowisko)
+
+class OsobaAdmin(admin.ModelAdmin):
+    readonly_fields = ('data_dodania',)  # Pole tylko do odczytu
--- Projekt_testowanie/Testowanie_app/models.py
+++ Projekt_testowanie/Testowanie_app/models.py
@@ -11,24 +11,24 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
+
 class Stanowisko(models.Model):
     nazwa = models.CharField(max_length=100, blank=False, null=False)
     opis = models.TextField(blank=True, null=True)
 
-    def __str__(self):
-        return self.nazwa
-
 class Osoba(models.Model):
-    PLEC_CHOICES = [
-        ('K', 'Kobieta'),
-        ('M', 'Mężczyzna'),
-        ('I', 'Inne'),
-    ]
+    class Plec(models.IntegerChoices):
+        KOBIETA = 1, 'Kobieta'
+        MEZCZYZNA = 2, 'Mężczyzna'
+        INNE = 3, 'Inne'
 
     imie = models.CharField(max_length=50, blank=False, null=False)
     nazwisko = models.CharField(max_length=50, blank=False, null=False)
-    plec = models.CharField(max_length=1, choices=PLEC_CHOICES)
+    plec = models.IntegerField(choices=Plec.choices)
     stanowisko = models.ForeignKey(Stanowisko, on_delete=models.CASCADE)
+    data_dodania = models.DateField(auto_now_add=True)
+
 
     def __str__(self):
         return f"{self.imie} {self.nazwisko}"
